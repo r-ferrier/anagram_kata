dd7cc15e327917853d0be749441e0034af6897e2
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2020-05-19 17:09:43 +0100

    counts how many words are in a list

--- lib/anagram_computer.rb
+++ lib/anagram_computer.rb
@@ -3,4 +3,8 @@ class AnagramComputer
     def word_list
       File.read("./wordlist.txt").split
     end
+
+    def word_count
+        word_list.length
+    end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,21 +2,29 @@ require 'anagram_computer.rb'
 
 RSpec.describe AnagramComputer do
 
+    before (:each) do
+        @anagram_computer = AnagramComputer.new
+    end
+
+    def word_list
+        File.read("./wordlist.txt").split
+    end
+
     it 'creates an anagram computer class' do
-      anagram_computer = AnagramComputer.new
-      expect(anagram_computer).to be_kind_of(AnagramComputer)
+      expect(@anagram_computer).to be_kind_of(AnagramComputer)
     end    
 
     it 'has a list of words' do
-      ac = AnagramComputer.new
-      expect(ac.word_list).to be_kind_of(Array) 
+      expect(@anagram_computer.word_list).to be_kind_of(Array) 
     end
 
     it 'reads words from a text file' do
-      ac = AnagramComputer.new
-      word_list = File.read("./wordlist.txt").split
-      
-      expect(ac.word_list).to eq(word_list)
+      expect(@anagram_computer.word_list).to eq(word_list)
     end
+
+    it 'counts how many words are in a text file' do
+      expect(@anagram_computer.word_count).to eq(word_list.length)
+    end
+
 
 end
